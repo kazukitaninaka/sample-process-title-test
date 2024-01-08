b9d60a8936af728d3dc013f691e5f5ad32701dc5
kazukitaninaka <onelive555@gmail.com>
2024-01-08 21:53:07 +0900

    initial commit

--- index-vitest.test.js
+++ index-vitest.test.js
@@ -2,7 +2,6 @@ import { test, expect } from "vitest";
 import { getProcessTitle } from "./index";
 
 test("test", () => {
-  console.log("RUNNING!!! test");
   process.title = "browser";
   const env = getProcessTitle();
   expect(env).toBe("browser");
